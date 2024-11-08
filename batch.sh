183d8777d3f3d4a482900199ef746f973f5f4520
thanadol-git <khunkoei@gmail.com>
2024-11-08 14:36:08 +0100

    add sort

--- batch.sh
+++ batch.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 # Directory containing .mzML files
-MZML_DIR="./sample_data/Andrea"
-# Directory containing target files (CSV)
-TARGET_DIR="./sample_data/Andrea"
+MZML_DIR="./2024 Khue temp"
+# Directory containing target files is same as mzmldir
+TARGET_DIR=$MZML_DIR
+
 # Target file (assuming it’s the same for all .mzML files)
-TARGET_FILE="$TARGET_DIR/Transitions-ApoEdge.csv"
+TARGET_FILE="$TARGET_DIR/Vebios_ComplemEdge_deepmrm_20241108.csv"
 # Calculate half of the available cores (rounded down)
 TOTAL_CORES=$(nproc)
 #CORES=$((TOTAL_CORES / 2))
@@ -23,6 +24,12 @@ process_mzml() {
 export -f process_mzml
 # Export the TARGET_FILE variable so it’s available in the parallel execution environment
 export TARGET_FILE
-# Run the command in parallel for all .mzML files in the directory
-find "$MZML_DIR" -name "*.mzML" | parallel -j "$CORES" process_mzml
-#echo “All predictions have been completed for all .mzML files using $CORES out of $TOTAL_CORES cores.”
+# Run the command in parallel for all .mzML files in the directory by sorting the file names
+# and passing them to parallel
+find "$MZML_DIR" -name "*.mzML" | sort | parallel -j "$CORES" process_mzml
+
+# find "$MZML_DIR" -name "*.mzML" | parallel -j "$CORES" process_mzml
+
+# Inform the user that all predictions have been completed after the last mzml 
+# file has been processed
+echo "All predictions completed"
